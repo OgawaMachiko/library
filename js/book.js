5dd189c358a34f9be498f7ed981f9998507602ed
okumura.tomohisa.2pf <okumura.tomohisa.2pf@jp.nssol.nipponsteel.com>
2023-12-14 12:00:16 +0900

    book.jsとbook.htmlの修正

--- js/book.js
+++ js/book.js
@@ -2,24 +2,21 @@ const book = Vue.createApp({
     data() {
       return {
         book: null,
-        comments: [],
-        explanations: [],
-        bookId:null,
-        employeeId:null
-      };
+        records: [],
+        employees: [],
+        bookId:1,
+      }
     },
     mounted() {
-      var params = new URLSearchParams(window.location.search);
-      this.bookId = parseInt(params.get('id'));
+      // var params = new URLSearchParams(window.location.search);
+      // this.bookId = parseInt(params.get('id'));
 
-      fetch(`http://localhost:3000/book`) 
+      fetch(`http://localhost:3000/books`) 
         .then(response => response.json())
         .then(data => {
-          const matchingBook = data.find(book => book.id === this.bookId);
+          const matchingBook = data.find(book => book.book_id === this.bookId);
           if (matchingBook) {
             this.book = matchingBook;
-            this.comments = matchingBook.comments;
-            this.explanations = matchingBook.explanations
           } else {
             console.error('Book not found.');
           }
@@ -28,13 +25,41 @@ const book = Vue.createApp({
           console.error('Error fetching data:', error);
         });
 
+        fetch(`http://localhost:3000/records`) 
+        .then(response => response.json())
+        .then(data => {
+          const matchingRecords = data.filter(records => records.book_id === this.bookId);
+          if (matchingRecords.length > 0) {
+            this.records = matchingRecords;
+          } else {
+            console.error('Record not found.');
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+
+        fetch(`http://localhost:3000/employees`) 
+        .then(response => response.json())
+        .then(data => {
+          this.employees = data;
+          // const matchingEmployees = data.filter(employees => records.book_id === this.bookId);
+          // if (matchingEmployees.length > 0) {
+          //   this.employees = matchingEmployees;
+          // } else {
+          //   console.error('Employee not found.');
+          // }
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
         
       },
       
 
     methods: {
         returnTop(id){
-            window.location.href="http://127.0.0.1:3000/index.html"
+          window.location.href = "http://127.0.0.1:3000/index.html"
         },
         handleRowClickEmp(employeeId){
           window.location.href = 'http://127.0.0.1:3000/index.html?id=' + employeeId;
@@ -45,6 +70,10 @@ const book = Vue.createApp({
         goBack() {
           window.history.back();
         },  
+        getUserName(empId) {
+          const employee = this.employees.find(emp => emp.emp_id === empId);
+          return employee ? employee.user_name : 'Unknown';
+        },
     },
   });
   book.mount('#book');
